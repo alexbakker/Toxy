535af28d3b3da0ba3f7458deb86ac5a69074f16d
Derrick- <derrick@alienseed.com>
2014-10-06 21:32:19 -0400

    Refactor Friend and Group models: extract new base class BaseChatModelView

--- Toxy/ViewModels/FriendControlModelView.cs
+++ Toxy/ViewModels/FriendControlModelView.cs
@@ -10,7 +10,7 @@ using Toxy.MVVM;
 
 namespace Toxy.ViewModels
 {
-    public class FriendControlModelView : ViewModelBase, IFriendObject
+    public class FriendControlModelView : BaseChatModelView, IFriendObject
     {
         public FriendControlModelView(MainWindowViewModel mainViewModel)
         {
@@ -96,7 +96,7 @@ namespace Toxy.ViewModels
 
         private bool selected;
 
-        public bool Selected
+        public override bool Selected
         {
             get { return this.selected; }
             set
@@ -114,141 +114,6 @@ namespace Toxy.ViewModels
                 if (action != null)
                 {
                     action(this, value);
-                }
-            }
-        }
-
-        private int chatNumber;
-
-        public int ChatNumber
-        {
-            get { return this.chatNumber; }
-            set
-            {
-                if (!Equals(value, this.ChatNumber))
-                {
-                    this.chatNumber = value;
-                    this.OnPropertyChanged(() => this.ChatNumber);
-                }
-            }
-        }
-
-        private ToxUserStatus toxStatus;
-
-        public ToxUserStatus ToxStatus
-        {
-            get { return this.toxStatus; }
-            set
-            {
-                if (!Equals(value, this.ToxStatus))
-                {
-                    this.toxStatus = value;
-                    this.OnPropertyChanged(() => this.ToxStatus);
-                }
-            }
-        }
-
-        private string statusMessage;
-
-        public string StatusMessage
-        {
-            get { return this.statusMessage; }
-            set
-            {
-                if (!Equals(value, this.StatusMessage))
-                {
-                    this.statusMessage = value;
-                    this.OnPropertyChanged(() => this.StatusMessage);
-                }
-            }
-        }
-
-        private string additionalInfo;
-
-        public string AdditionalInfo
-        {
-            get { return this.additionalInfo; }
-            set
-            {
-                if (!Equals(value, this.AdditionalInfo))
-                {
-                    this.additionalInfo = value;
-                    this.OnPropertyChanged(() => this.AdditionalInfo);
-                }
-            }
-        }
-
-        private string name;
-
-        public string Name
-        {
-            get { return this.name; }
-            set
-            {
-                if (!Equals(value, this.Name))
-                {
-                    this.name = value;
-                    this.OnPropertyChanged(() => this.Name);
-                }
-            }
-        }
-
-        private bool hasNewMessage;
-
-        public bool HasNewMessage
-        {
-            get { return this.hasNewMessage; }
-            set
-            {
-                if (!Equals(value, this.HasNewMessage))
-                {
-                    this.hasNewMessage = value;
-                    this.OnPropertyChanged(() => this.HasNewMessage);
-                }
-            }
-        }
-
-        private ImageSource avatar;
-
-        public ImageSource Avatar
-        {
-            get { return this.avatar; }
-            set
-            {
-                if (!Equals(value, this.Avatar))
-                {
-                    this.avatar = value;
-                    this.OnPropertyChanged(() => this.Avatar);
-                }
-            }
-        }
-
-        private byte[] avatarBytes;
-
-        public byte[] AvatarBytes 
-        {
-            get { return avatarBytes; }
-            set
-            {
-                if (!Equals(value, this.AvatarBytes))
-                {
-                    this.avatarBytes = value;
-                    this.OnPropertyChanged(() => this.AvatarBytes);
-                }
-            }
-        }
-
-        private int newMessageCount;
-
-        public int NewMessageCount
-        {
-            get { return this.newMessageCount; }
-            set
-            {
-                if (!Equals(value, this.NewMessageCount))
-                {
-                    this.newMessageCount = value;
-                    this.OnPropertyChanged(() => this.NewMessageCount);
                 }
             }
         }

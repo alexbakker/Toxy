7efd8a2abfe195056c4a25f1b8bef2ca91b05709
Impyy <impyy@tox.im>
2015-02-04 16:31:04 +0100

    Added '/nightly' argument to the updater

--- Toxy.Updater/Program.cs
+++ Toxy.Updater/Program.cs
@@ -24,13 +24,14 @@ namespace Toxy.Updater
         static string _updateFileName = "update.zip";
         static string _publicKey = "3082010A0282010100F7E965CEDCC8B57C8849D68D0BA0E060A3E1EA952616D06A431B9D50D58F936ADEE71F7C15CC13C757F63766C750D180B4B868954D2C6A576CB27B9ECF4EE06FBEA54A8307973D56E468482966D6653B238BA681526AC01DDB5858FF0855896D9365E4007C0ED5A321E99F4210B3C976C383059E557056F0FA7311417273F04B8A207CDD8FF3DC89FDA505B337CE1C27BA46F176D5BA2B45BBAC35D06182909249BDEB835232B282B8F7FAB2C19720D90F1B3B677922C87DF503E86EADADC7C4CD63744F549279F9A4429CE4D745B0F36DDA2FA01107A1C142BE6C09AB58E631CC9756D831302044EACE9D71864E8566D95952A53880A64C875472F82E2251F90203010001";
 
+        static bool _forceNightly = false;
         static bool _forceUpdate = false;
         static bool _finished = false;
 
         static List<string> _extractedFiles = new List<string>();
         static string _jsonUri = "http://toxy.content.impy.me/versions";
 
-        static bool isX64
+        static bool _isX64
         {
             get
             {
@@ -41,11 +42,11 @@ namespace Toxy.Updater
             }
         }
 
-        static string nightlyUri
+        static string _nightlyUri
         {
             get
             {
-                if (isX64)
+                if (_isX64)
                     return "https://jenkins.impy.me/job/Toxy%20x64/lastSuccessfulBuild/artifact/toxy_x64.zip";
                 else
                     return "https://jenkins.impy.me/job/Toxy%20x86/lastSuccessfulBuild/artifact/toxy_x86.zip";
@@ -57,10 +58,14 @@ namespace Toxy.Updater
             ProcessArguments(args);
 
             string currentVersion = GetToxyVersion();
-            if (_forceUpdate)
+            if (_forceNightly)
+            {
+                RunUpdate(_nightlyUri);
+            }
+            else if (_forceUpdate)
             {
                 var latest = GetLatestVersion();
-                RunUpdate(isX64 ? (string)latest["url_x64"] : (string)latest["url_x86"]);
+                RunUpdate(_isX64 ? (string)latest["url_x64"] : (string)latest["url_x86"]);
             }
             else if (string.IsNullOrEmpty(currentVersion))
             {
@@ -68,7 +73,7 @@ namespace Toxy.Updater
                 if (result == DialogResult.Yes)
                 {
                     var latest = GetLatestVersion();
-                    RunUpdate(isX64 ? (string)latest["url_x64"] : (string)latest["url_x86"]);
+                    RunUpdate(_isX64 ? (string)latest["url_x64"] : (string)latest["url_x86"]);
                 }
             }
             else
@@ -78,7 +83,7 @@ namespace Toxy.Updater
                 {
                     var latest = GetLatestVersion();
                     if (new Version(currentVersion) < new Version((string)latest["version"]))
-                        RunUpdate(isX64 ? (string)latest["url_x64"] : (string)latest["url_x86"]);
+                        RunUpdate(_isX64 ? (string)latest["url_x64"] : (string)latest["url_x86"]);
                     else
                     {
                         if (File.Exists(Path.Combine(_path, "Toxy.exe")))
@@ -152,6 +157,9 @@ namespace Toxy.Updater
                     case "/force":
                     case "/f":
                         _forceUpdate = true;
+                        break;
+                    case"/nightly":
+                        _forceNightly = true;
                         break;
                 }
             }

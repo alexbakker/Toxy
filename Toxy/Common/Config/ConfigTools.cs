6111ac55a218a859c4027e5b0e6936ac7c47f77a
BasedMan <BM-2cUfK8wDkfSd9QnvYX4mJH5fpFEHTgd6jp@bitmessage.ch>
2015-05-14 20:09:36 +0200

    ConfigTools: Instantiate FileStreams in using statements

--- Toxy/Common/Config/ConfigTools.cs
+++ Toxy/Common/Config/ConfigTools.cs
@@ -7,22 +7,20 @@ namespace Toxy.Common
     {
         public static void Save(Config config, string filename)
         {
-            FileStream stream = new FileStream(filename, FileMode.Create);
-            XmlSerializer serializer = new XmlSerializer(typeof(Config));
-
-            serializer.Serialize(stream, config);
-            stream.Dispose();
+            using (FileStream stream = new FileStream(filename, FileMode.Create))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(Config));
+                serializer.Serialize(stream, config);
+            }
         }
 
         public static Config Load(string filename)
         {
-            FileStream stream = new FileStream(filename, FileMode.Open);
-            XmlSerializer serializer = new XmlSerializer(typeof(Config));
-
-            Config config = (Config)serializer.Deserialize(stream);
-            stream.Dispose();
-
-            return config;
+            using (FileStream stream = new FileStream(filename, FileMode.Open))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(Config));
+                return (Config)serializer.Deserialize(stream);
+            }
         }
     }
 }

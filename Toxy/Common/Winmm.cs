da96df88115f33aa3ceb09a7e06b2fc220f876b5
BasedMan <BM-2cUfK8wDkfSd9QnvYX4mJH5fpFEHTgd6jp@bitmessage.ch>
2015-05-14 22:08:17 +0200

    Simplify audio notification

--- Toxy/Common/Winmm.cs
+++ Toxy/Common/Winmm.cs
@@ -9,31 +9,17 @@ namespace Win32
     {
         public static void PlayMessageNotify()
         {
-            PlayWavResource("Resources.Audio.Blop.wav");
+            using (UnmanagedMemoryStream sound = Toxy.Properties.Resources.Blop)
+            {
+                byte[] soundData = new byte[sound.Length];
+                sound.Read(soundData, 0, (int) sound.Length);
+                PlaySound(soundData, IntPtr.Zero, SND_ASYNC | SND_MEMORY);
+            }
         }
 
-        private static byte[] resourceStreamLength;
         private const UInt32 SND_ASYNC = 1;
         private const UInt32 SND_MEMORY = 4;
         [DllImport("Winmm.dll")]
         private static extern bool PlaySound(byte[] data, IntPtr hMod, UInt32 dwFlags);
-
-        private static void PlayWavResource(string wav)
-        {
-            var strNameSpace = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
-
-            if (resourceStreamLength == null)
-            {
-                var resourceStream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream(strNameSpace + "." + wav);
-                if (resourceStream != null)
-                {
-                    resourceStreamLength = new Byte[resourceStream.Length];
-                    resourceStream.Read(resourceStreamLength, 0, (int)resourceStream.Length);
-                }
-            }
-
-            if (resourceStreamLength != null)
-                PlaySound(resourceStreamLength, IntPtr.Zero, SND_ASYNC | SND_MEMORY);
-        }
     }
 }

2ebd7de4589cfb9fd6a44282d73b722b05759600
Impyy <alex@impy.me>
2015-08-31 16:19:20 +0200

    Only debug builds should attempt to write to the debug log

--- Toxy/Debugging.cs
+++ Toxy/Debugging.cs
@@ -5,6 +5,7 @@ namespace Toxy
 {
     public static class Debugging
     {
+        [Conditional("DEBUG")]
         public static void Write(string text, [CallerFilePath] string filename = "", [CallerMemberName] string member = "", [CallerLineNumber] int line = 0)
         {
             Debug.WriteLine("{0}_{1}({2}): {3}", Path.GetFileName(filename), member, line, text);

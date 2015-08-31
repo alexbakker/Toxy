4284c60419a26e71524e2ead1c2ee5e87590e7ea
Impyy <alex@impy.me>
2015-08-31 18:59:15 +0200

    Fixed login window style

---
+++ Toxy/Converters/BoolToVisibilityConverter2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Toxy.Converters
+{
+    public class BoolToVisibilityConverter2 : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            bool selected = (bool)value;
+            return selected ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
